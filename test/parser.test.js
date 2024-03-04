3d5708a168b6910562faa83cf843b012edb0e18d
marco-ippolito <marcoippolito54@gmail.com>
2024-03-04 12:35:41 +0100

    milo parsing correctly

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,6 +1,40 @@
 import { load } from '../lib/milo.js';
+import { test } from 'node:test';
+import assert from 'node:assert';
 
-const parser = load();
-const request = Buffer.from('GET / HTTP/1.1\r\n\r\n')
-parser.parse(request, request.length);
-console.log(parser.context.method);
+
+test('Milo parsing http response', () => {
+    const protocol = "HTTP";
+    const version = "1.1";
+    const status = "200";
+    const contentType = "text/plain";
+    const contentLength = "15";
+    const body = "Hello, World!\r\n";
+    const request = Buffer.from(`${protocol}/${version} ${status} OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${contentLength}\r\n\r\n${body}`)
+
+    const parser = load();
+    parser.parse(request, request.length);
+    console.log(parser.context)
+    assert.strictEqual(parser.context.protocol, protocol);
+    assert.strictEqual(parser.context.version, version);
+    assert.strictEqual(parser.context.status, status);
+    assert.strictEqual(parser.context["Content-Type"], contentType);
+    assert.strictEqual(parser.context["Content-Length"], contentLength);
+    assert.strictEqual(parser.context.data, body);
+});
+
+test('Milo parsing http request', () => {
+    const method = "GET";
+    const url = "/";
+    const protocol = "HTTP";
+    const version = "1.1";
+    const request = Buffer.from(`${method} ${url} ${protocol}/${version}\r\n\r\n`)
+
+    const parser = load();
+    parser.parse(request, request.length);
+    console.log(parser.context)
+    assert.strictEqual(parser.context.method, method);
+    assert.strictEqual(parser.context.url, url);
+    assert.strictEqual(parser.context.protocol, protocol);
+    assert.strictEqual(parser.context.version, version);
+});
