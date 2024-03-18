03f2725310a5e9e24d69e8d885ac53029718c993
marco-ippolito <marcoippolito54@gmail.com>
2024-03-18 12:57:28 +0100

    fix: renamed to InboundTCPSocket

--- test/socket.test.js
+++ test/socket.test.js
@@ -1,10 +1,10 @@
-import { InboundSocket } from '../lib/socket.js';
+import { InboundTCPSocket } from '../lib/socket.js';
 import { test } from 'node:test';
 import assert from 'node:assert';
 
-test('InboundSocket', async () => {
+test('InboundTCPSocket', async () => {
     const body = "Hello, World!\r\n";
-    const socket = new InboundSocket({ hostname: "0.0.0.0", port: 8000 });
+    const socket = new InboundTCPSocket({ hostname: "0.0.0.0", port: 8000 });
     socket.listen((req, res, err) => {
         assert.ok(req.data.includes(body));
         const http_response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\n${body}`;
